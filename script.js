e33315695d23e5fa040426c52ab65a5781f52c6c
Sean Boonzaier <boonzaiersa@gmail.com>
2022-08-13 09:15:42 +0200

    Add skeleton modules for boardgame

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
+const board = document.getElementById("board");
+
+
+
+
 //Object for gameboard using module because only 1
+const gameBoard = ( () => {
+    let boardArray = [0, 0, 0, 0, 0, 0];
 
 
+    const generateBoard = () => {
+        let boardTable = document.createElement('table');
+        let boardBody = document.createElement('tbody');
+
+        for(let i = 0; i < 3; i++){
+            let tableRow = document.createElement('tr');
+            let cell1 = document.createElement('td');
+        }
+
+        boardTable.appendChild(boardBody);
+        board.appendChild(boardTable);
+
+    }
+
+    return {generateBoard};
+})();
 
 //Object for displaying things using module because only 1
 
